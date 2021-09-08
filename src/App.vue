f5eed3ee5275bb6bc589bf2c7b1bb699420b976c
abako123 <babyabacus1@gmail.com>
2021-09-08 22:59:25 +0900

    13 menu group

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,28 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon @click="drawer = !drawer"/>
       <site-title :title="title"></site-title>
       <v-spacer/>
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
       <v-divider></v-divider>
-
       <site-menu></site-menu>
     </v-navigation-drawer>
 
     <v-main>
-      <router-view></router-view>
+      <router-view/>
     </v-main>
 
     <site-footer :footer="footer"></site-footer>
-
   </v-app>
 </template>
 
 <script>
-import SiteTitle from '@/views/site/title'
-import SiteFooter from '@/views/site/footer'
-import SiteMenu from '@/views/site/menu'
+import SiteTitle from './views/site/title'
+import SiteFooter from './views/site/footer'
+import SiteMenu from './views/site/menu'
 
 export default {
   components: { SiteTitle, SiteFooter, SiteMenu },
