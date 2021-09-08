dd08ae92a8242dc1ea6c327282d5d89da51b4fb1
abako123 <babyabacus1@gmail.com>
2021-09-08 18:12:07 +0900

    11 add

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,65 @@
       color="primary"
       dark
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"/>
       <v-toolbar-title>Page title</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon to="/about">
+
+      <v-btn icon to="/xxx">
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-      <v-btn icon to="/">
+      <v-btn icon to="/about">
         <v-icon>mdi-heart</v-icon>
       </v-btn>
-      <v-btn icon>
+      <v-btn icon to="/">
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
 
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
       <router-view></router-view>
     </v-main>
+
+    <v-footer app color="primary" dark fixed>
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }} ABAKO-Simsan</div>
+    </v-footer>
+
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
